e00a0b3413d3c3821e0d5b76a34a0b7c4ae2881b
Suresh Randawa <srandawa@gmail.com>
2024-12-05 18:14:50 -0500

    initial commit

---
+++ mpdfront/message.py
@@ -0,0 +1,22 @@
+import logging
+log = logging.getLogger(__name__)
+
+class QueueMessage:
+    _type = ""
+    _item = ""
+    _data = ""
+    def __init__(self, type:str=None, item:str=None, data=None):
+        if not type or not item:
+            raise ValueError("type, item are required arguments")
+        self._type = type
+        self._item = item
+        self._data = data
+
+    def get_type(self):
+        return self._type
+
+    def get_item(self):
+        return self._item
+
+    def get_data(self):
+        return self._data
